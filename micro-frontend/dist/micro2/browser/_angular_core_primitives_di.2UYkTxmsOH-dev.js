138f019f5d0d10688307c28c81f33ad50cf81cfb
mejo <mejo_jo@outlook.com>
2025-07-22 14:14:37 +0300

    up

--- micro-frontend/dist/micro2/browser/_angular_core_primitives_di.2UYkTxmsOH-dev.js
+++
@@ -1,3 +0,0 @@
-import { NOT_FOUND, NotFoundError, getCurrentInjector, isNotFound, setCurrentInjector } from "@nf-internal/chunk-WNSIDLSC";
-import "@nf-internal/chunk-WDMUDEB6";
-export { NOT_FOUND, NotFoundError, getCurrentInjector, isNotFound, setCurrentInjector };
--- micro-frontend/shell/src/main.ts
+++
@@ -1,7 +0,0 @@
-// src/main.ts
-import { initFederation } from '@angular-architects/native-federation';
-
-initFederation('/assets/federation.manifest.json')
-  .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
-  .catch(err => console.error(err));
